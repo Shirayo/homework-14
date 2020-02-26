edd078c0fd34ed7cb138527874fdfe8b06a9c20e
Vasily <iphoje@gmail.com>
2020-02-27 01:42:33 +0300

    Initial Commit

---
+++ homework №14/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework №14
+//
+//  Created by Shirayo on 27.02.2020.
+//  Copyright © 2020 Shirayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
